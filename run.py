1270a27a0205e296de0fd29c0f5b166c5ba584ff
amoghjain <amoghjainsp@gmail.com>
2018-06-27 12:29:56 +0530

    initial commit for preprocessing studio audio files

--- run.py
+++ run.py
@@ -3,33 +3,44 @@ import parse_pocketsphinx as parse_ps
 import generate_reports as ps_reports
 import os
 import constants as cs
+import sys
 
 f_preprocess_report = os.path.join(os.getcwd(), "preprocessing_report.tsv")
 lang = "hindi"
 lang_code = cs.Hindi
 
 if True:
-    # Preprocessing of Audio Files
-    users_list = []
-        # ["amit.singh@reverieinc.com", "minakshi.sablok@reverieinc.com", "ranvijai.singh@reverieinc.com",
-        #           "sangita.ekka@reverieinc.com", "sneha.chandran@reverieinc.com", "tarun.agarwal@reverieinc.com",
-        #           "gitika.handique@reverieinc.com"]
-    report_writer = open(f_preprocess_report, "w")
-    for usr in usr_aud_ts_files.get_users_from_db():
-        if usr not in users_list:
-            usr_aud_ts_files.init(usr)
-            usr_aud_ts_files.download_audio_n_txt(usr, lang)
-            usr_aud_ts_files.trimming_audio()
-            usr_aud_ts_files.wav_to_raw()
-            # a = ['/home/pankaj/asr/AudioTranscriptVerifier/ranvijai.singh@reverieinc.com/2',
-            #  '/home/pankaj/asr/AudioTranscriptVerifier/ranvijai.singh@reverieinc.com/3',
-            #  '/home/pankaj/asr/AudioTranscriptVerifier/ranvijai.singh@reverieinc.com/1']
-            for val in usr_aud_ts_files.single_transcript():
-            # for val in a:
-                report_writer.write(os.path.dirname(usr_aud_ts_files.out_folder_path) + "\t")
-                report_writer.write(val.split(os.getcwd())[1])
-                report_writer.write("\n")
-            # usr_aud_ts_files.out_folder_path
+    arg1 = sys.argv[1]
+
+    if arg1 == "studio":
+        print("studio data is given")
+        in_folder = sys.argv[2]
+
+
+
+    elif arg1 == "recording":
+        print("studio data is given")
+        # Preprocessing of Audio Files
+        users_list = []
+            # ["amit.singh@reverieinc.com", "minakshi.sablok@reverieinc.com", "ranvijai.singh@reverieinc.com",
+            #           "sangita.ekka@reverieinc.com", "sneha.chandran@reverieinc.com", "tarun.agarwal@reverieinc.com",
+            #           "gitika.handique@reverieinc.com"]
+        report_writer = open(f_preprocess_report, "w")
+        for usr in usr_aud_ts_files.get_users_from_db():
+            if usr not in users_list:
+                usr_aud_ts_files.init(usr)
+                usr_aud_ts_files.download_audio_n_txt(usr, lang)
+                usr_aud_ts_files.trimming_audio()
+                usr_aud_ts_files.wav_to_raw()
+                # a = ['/home/pankaj/asr/AudioTranscriptVerifier/ranvijai.singh@reverieinc.com/2',
+                #  '/home/pankaj/asr/AudioTranscriptVerifier/ranvijai.singh@reverieinc.com/3',
+                #  '/home/pankaj/asr/AudioTranscriptVerifier/ranvijai.singh@reverieinc.com/1']
+                for val in usr_aud_ts_files.single_transcript():
+                # for val in a:
+                    report_writer.write(os.path.dirname(usr_aud_ts_files.out_folder_path) + "\t")
+                    report_writer.write(val.split(os.getcwd())[1])
+                    report_writer.write("\n")
+                # usr_aud_ts_files.out_folder_path
 
 if True:
     # Using PocketSphinx to generate reports
